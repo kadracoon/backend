549474a949f8ffb98745fbbfc830645dc33fa5c5
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-11-16 05:30:26 +0500

    CORS updated

--- app/main.py
+++ app/main.py
@@ -22,6 +22,8 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://kadracoon-frontend.vercel.app",
+    "https://kadracoon.fun",
 ]
 
 app.add_middleware(
